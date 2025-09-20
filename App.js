f2d2b380e240fac431c579c0380eca5bf4daa228
Jhunryl04 <jhunryldelacruz@gmail.com>
2025-09-20 13:09:41 +0800

    add

---
+++ App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
+import CounterApp from './CounterApp';
+import ColorChangeApp from './ColorChangeApp';
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <CounterApp />
+        <ColorChangeApp />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
